4fa937249dcf2bd0b1709c2440c4e6511f558d7d
y-sathwik <PDevSathwik@yathisolutions.com>
2025-05-17 22:24:23 +0530

    fix(form): memoize context value using useMemo to avoid unnecessary re-renders

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -33,8 +33,10 @@ const FormField = <
 >({
   ...props
 }: ControllerProps<TFieldValues, TName>) => {
+  const memoizedValue = React.useMemo(() => ({ name: props.name }), [props.name])
+
   return (
-    <FormFieldContext.Provider value={{ name: props.name }}>
+    <FormFieldContext.Provider value={memoizedValue}>
       <Controller {...props} />
     </FormFieldContext.Provider>
   )
@@ -71,9 +73,10 @@ const FormItemContext = React.createContext<FormItemContextValue>({} as FormItem
 
 function FormItem({ className, ...props }: React.ComponentProps<'div'>) {
   const id = React.useId()
+  const memoizedValue = React.useMemo(() => ({ id }), [id])
 
   return (
-    <FormItemContext.Provider value={{ id }}>
+    <FormItemContext.Provider value={memoizedValue}>
       <div data-slot="form-item" className={cn('grid gap-2', className)} {...props} />
     </FormItemContext.Provider>
   )
